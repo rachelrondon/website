f98bed695f80a6c2b9f664d748ca75f47e23541b
Rachel Rondon <rachel.sun.rondon@gmail.com>
2021-02-21 16:20:00 -0500

    new portfolio website

---
+++ src/nav.js
@@ -0,0 +1,30 @@
+import React, { useState, useEffect } from 'react';
+import Logo from './logo.png';
+
+const NavBar = () => {
+  const [links, setLinks] = useState([
+    { title: "LINKEDIN",
+      link: "https://www.linkedin.com/in/rachelrondon/"
+    },
+    { title: "GITHUB",
+      link: "https://github.com/rachelrondon",
+    }
+  ])
+
+  return (
+    <nav className="nav">
+      <p className="nav-logo">Rachel Rondon</p>
+      <ul className="nav-links">
+        {links.map((link) => {
+          return (
+            <li className="nav-link"><a href={link.link}>{link.title}</a></li>
+          )
+        })}
+      </ul>
+    </nav>
+  )
+}
+
+
+
+export default NavBar;
---
+++ src/portfolio.js
@@ -0,0 +1,69 @@
+import React from 'react';
+import CssIcon from './css3_icon.svg';
+import Html5Icon from './html5_color.svg';
+import JsIcon from './js_color.svg';
+import ReactIcon from './react_color.svg';
+import SoundCloudIcon from './sc_circle.svg';
+import ImageOne from './image-one.jpg';
+import ImageTwo from './image-two.jpg';
+
+class Portfolio extends React.Component {
+  constructor() {
+    super();
+
+    this.state = {
+      projects: [
+        {
+          title: 'Daily Routine x React',
+          description: 'Daily Routine is a fitness website that I designed and developed that allows users to view curated yoga routines set to popular music. The SoundCloud API was utilized for this website.',
+          image: ImageOne,
+          icon: CssIcon,
+          iconOne: Html5Icon,
+          iconTwo: JsIcon,
+          iconThree: ReactIcon,
+          iconFour: SoundCloudIcon,
+          link: "https://github.com/rachelrondon/infinite_yoga-"
+        },
+        {
+          title: 'House Keys x React',
+          description: 'House Keys is a roommate finder website that I created with a team of developers and utilizes the Google Maps API.',
+          image: ImageTwo,
+          icon: CssIcon,
+          iconOne: Html5Icon,
+          iconTwo: JsIcon,
+          iconThree: ReactIcon,
+          iconFour: "",
+          link: "https://github.com/housekeys"
+        },
+      ],
+    }
+  }
+
+  render() {
+    return (
+      <div className="portfolio">
+        {this.state.projects.map((project) => {
+          return (
+            <div className="card">
+              <img className="card-image" src={project.image}></img>
+              <div className="card-details">
+                <p className="card-title">{project.title}</p>
+                <ul className="card-icons">
+                  <li><img className="card-icon" src={project.icon}></img></li>
+                  <li><img className="card-icon" src={project.iconOne}></img></li>
+                  <li><img className="card-icon" src={project.iconTwo}></img></li>
+                  <li><img className="card-icon" src={project.iconThree}></img></li>
+                  <li><img className="card-icon" src={project.iconFour}></img></li>
+                </ul>
+                <p className="card-description">{project.description}</p>
+                <a className="card-link" href={project.link} target="_blank">GITHUB</a>
+              </div>
+            </div>
+          )
+        })}
+      </div>
+    )
+  }
+}
+
+export default Portfolio;
